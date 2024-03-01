e4b1033e29faf668c31b632f13b707b61b05d219
VenkateshTalabattula <161871709+VenkateshTalabattula@users.noreply.github.com>
2024-03-01 23:21:32 +0530

    Add2Nums.cs

---
+++ Add2Nums.cs
@@ -0,0 +1,8 @@
+int a,b,c;
+Console.Writeline("Enter a A Number");
+a= Convert.ToInt32(Console.ReadLine());
+Console.Writeline("Enter a B Number");
+b= Convert.ToInt32(Console.ReadLine());
+c=a+b;
+c=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
